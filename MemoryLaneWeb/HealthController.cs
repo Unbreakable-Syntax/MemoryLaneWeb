d7b959292c18a0cfafa86ced2934ef5f199193b3
Percival <louwenngatusani@yahoo.com>
2026-05-09 09:57:29 +0800

    Added a quick ping endpoint to push the server alive

---
+++ MemoryLaneWeb/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MemoryLaneWeb
+{
+    [ApiController]
+    [Route("api/")]
+    public class HealthController : ControllerBase
+    {
+
+        [HttpGet("health")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Server active!");
+        }
+    }
+}
